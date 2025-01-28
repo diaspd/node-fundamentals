0e71fb7c3f6d8460a1594758a88511402145f78e
Pedro Dias <pdr050203@gmail.com>
2025-01-27 22:09:42 -0300

    feat: add structure from app

--- src/server.js
+++ src/server.js
@@ -1,7 +1,28 @@
 import http from 'node:http'
 
+// GET, POST, PUT, PATCH, DELETE
+
+// GET => Buscar um recurso do back-end
+// POST => Criar um recurso no back-end
+// PUT => Atualizar um recurso no back-end
+// PATCH => Atualizar uma informação específica de um recurso no back-end
+// DELETE => Deletar um recurso do back-end
+
+// GET /users => Buscando usuários no banc-end
+// POST /users => Criar um usuário no back-end
+
 const server = http.createServer((req, res) => {
-  return res.end('Hello Ignite')
+  const { method, url } = req
+
+  if (method === 'GET' && url === '/users') {
+    return res.end('Listagem de usuários')
+  }
+
+  if (method === 'POST' && url === '/users') {
+    return res.end('Criação de usuário')
+  }
+
+  return res.end('Hello Word')
 })
 
 server.listen(3333)
