f75133091feeb961282e06b224067af68aaded3e
Pedro Dias <pdr050203@gmail.com>
2025-02-05 20:25:19 -0300

    feat: add database

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import http from 'node:http'
+import { Database } from './database.js'
 import { json } from './middlewares/json.js'
 
 // - Criar usuários
@@ -27,7 +28,7 @@ import { json } from './middlewares/json.js'
 
 // HTTP Status Code
 
-const users = []
+const database = new Database()
 
 const server = http.createServer(async (req, res) => {
   const { method, url } = req
@@ -35,18 +36,21 @@ const server = http.createServer(async (req, res) => {
   await json(req, res)
 
   if (method === 'GET' && url === '/users') {
-    return res
-      .end(JSON.stringify(users))
+    const users = database.select('users')
+
+    return res.end(JSON.stringify(users))
   }
 
   if (method === 'POST' && url === '/users') {
     const { name, email } = req.body
 
-    users.push({
+    const user = {
       id: 1,
       name,
       email,
-    })
+    }
+
+    database.insert('users', user)
 
     return res.writeHead(201).end()
   }
